bafbb7954c690f416daaae99aa14f86cba08a2f0
Kauã Borgarelli <kaua.tavares@fatec.sp.gov.br>
2024-02-21 21:20:58 -0300

    First commit

---
+++ src/main/java/br/sp/gov/br/SpringBoot3LabIV/SpringBoot3LabIvApplication.java
@@ -0,0 +1,13 @@
+package br.sp.gov.br.SpringBoot3LabIV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3LabIvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot3LabIvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/sp/gov/br/SpringBoot3LabIV/SpringBoot3LabIvApplicationTests.java
@@ -0,0 +1,13 @@
+package br.sp.gov.br.SpringBoot3LabIV;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3LabIvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
